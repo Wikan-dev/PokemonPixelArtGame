3193c7becea01fed42704eb6269a0e7f6bb0d263
Liningini <icelyindo876@gmail.com>
2025-09-08 12:54:29 +0800

    Added the thing part3

--- src/home.jsx
+++ src/home.jsx
@@ -4,7 +4,10 @@ const Home = () => {
     return (
         <div>
             <img src={homeImg} alt="backgound Image" className="w-full h-[100vh] object-cover"/>
-            <img src={PK} alt="PokePixel logo" className=""/>
+            <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
+                <img src={PK} alt="PokePixel logo" className="mt-[-500px]"/>
+                <p className="text-white text-lg font-bold mt-10">Paint Pokemon — One Color at a Time</p>
+            </div>    
         </div>
     )
 }
