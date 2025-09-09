e77b95c2d7eaf11b31cfc6035b4154d0faed529a
Wikan-dev <oneshootindo@gmail.com>
2025-09-09 17:47:32 +0800

    added drawing

--- src/mainPixelArt.jsx
+++ src/mainPixelArt.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const Pixel = ({ hover, click, count, pixelColor, pixels, setPixels}) => {
+const Pixel = ({ hover, click, pixelColor, pixels, setPixels}) => {
     // const [color, setColor] = useState(Array(count).fill(false));
     const bg = 'bg-black';
     // console.log(color);
@@ -23,10 +23,12 @@ const Pixel = ({ hover, click, count, pixelColor, pixels, setPixels}) => {
         }
     }
 
+    
+
     return (
         <div className="flex gap-1 w-195 flex-wrap">
             {pixels.map((color, i) => (
-                <div key={i} onClick={() => changeColorClick(i)} onMouseEnter={() => changeColor(i)} className={` outline-1 w-5 h-5`} style={{ backgroundColor : color   ? pixelColor : 'white'}}></div>
+                <div key={i} onClick={() => changeColorClick(i)} onMouseEnter={() => changeColor(i)} className={` outline-1 w-5 h-5`} style={{ backgroundColor : color ? pixelColor : 'rgba(0, 0, 0, 20%)'}}></div>
             ))}
         </div>
     )
@@ -44,8 +46,9 @@ const Pixel = ({ hover, click, count, pixelColor, pixels, setPixels}) => {
 const PixelArt = () => {
     const [hover, setHover] = useState(true);
     const [click, setClick] = useState(false);  
-    const [pixelColor, setPixelColor] = useState('#5a0fb0ff');
+    const [pixelColor, setPixelColor] = useState('');
     const [pixels, setPixels] = useState(Array(512).fill(''));
+    const color = ["#000000", "#ffffff", "#82c6f2", "#b2c6d9", "#ffb545", "#cf8b57", "#80806a", "#80806a"];
 
     function handleHover() {
         if (hover != true) {
@@ -57,6 +60,11 @@ const PixelArt = () => {
         }
     }
 
+    function getColor(col) {
+        // const selectedColor = JSON.stringify(color);
+        setPixelColor(col);
+    }
+
     function handleClick() {
         if (click != true) {
             setClick(true);
@@ -65,6 +73,10 @@ const PixelArt = () => {
             console.log('click is already true');
             return;
         }
+    }
+
+    function handleReset() {
+        setPixels(Array(512).fill(''));
     }
 
     // useEffect(() => {
@@ -83,10 +95,15 @@ const PixelArt = () => {
         <div>
             <Pixel hover={hover} click={click} count={512} pixelColor={pixelColor} pixels={pixels} setPixels={setPixels}/>
             {/* <PixelColor pixelColor={'#6912f7ff'}/> */}
-            <div className="w-100 h-100" style={{ backgroundColor : pixelColor}}></div>
+            <div className="flex gap-10">
+            {color.map((col, i) => (
+                <div key={i} onClick={() => getColor(col)} className="w-10 h-10 outline-2" style={{ backgroundColor : col}}></div>
+            ))}
+            </div>
             <div className="mt-10 flex gap-5">
                 <button onClick={handleHover} className={`${hover ? 'bg-white text-black' : 'bg-black text-white'} hover:scale-110 bg-black outline-2 w-20 h-10 p-2 font-bold rounded-lg`}>hover</button>
                 <button onClick={handleClick} className={`${click ? 'bg-white text-black' : 'bg-black text-white'} text-black w-20 h-10 p-2 outline-2 font-bold rounded-lg`}>click</button>
+                <button onClick={handleReset} className={`bg-red-500 text-white w-20 h-10 p-2 outline-2 font-bold rounded-lg`}>reset</button>
             </div>
         </div>
     )
