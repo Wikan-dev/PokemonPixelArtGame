aa2fab0520eecfd53d7bbc70ac6942a086450d5b
Wikan-dev <oneshootindo@gmail.com>
2025-09-15 16:29:09 +0800

    bug fix

--- src/test.jsx
+++ src/test.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Test = ({onSelectPokemon}) => {
     return (
-        <div>
+        <div className='flex mx-auto'>
             {data.pokemons.map((poke, idx) => (
                     <div onClick={() => onSelectPokemon(poke)} key={idx} className="flex flex-col items-center cursor-pointer" onMouseEnter={() => setSelected(poke)} onMouseLeave={() => setSelected(null)}>
                         <Link to='/pixel-art' state={{ pokemonId: idx,color: poke.colour, pokemon: poke["Pokemon-colour"]}} ><img src={poke.image} alt={`Pokemon ${idx}`} className="w-28 h-auto  opacity-50 hover:opacity-100"/></Link>
