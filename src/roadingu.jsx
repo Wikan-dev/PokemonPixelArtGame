313f65de225c6c3e1da036d4541a9a080efdc193
Wikan-dev <oneshootindo@gmail.com>
2025-09-16 12:02:54 +0800

    deploy 7

--- src/roadingu.jsx
+++ src/roadingu.jsx
@@ -5,7 +5,7 @@ const roadingu = () => {
             <img src={bggif} alt="Background Gif" className="w-full h-[100vh] object-cover"/>
             <div className="flex flex-col items-center min-h-screen absolute inset-0 mt-20">
                 <div className="text-outline pixel1 text-[22px] text-white mt-40 bg-[#333333] rounded-l-lg p-4 w-[412px] min-h-[189px] mr-39">
-                    <img src="/public/Yapping.png" alt="Pfor tolkien" className="absolute ml-99 mt-[-16px]" />
+                    <img src={`${import.meta.env.BASE_URL}Yapping.png`} alt="Pfor tolkien" className="absolute ml-99 mt-[-16px]" />
                     <p className="w-90 text-justify">May fortune be thy steadfast guide, leading thee to joyous endeavors aplenty!</p>
                     <br></br>
                     <p>The world of Pokemon awaits...</p>
--- src/test.jsx
+++ src/test.jsx
@@ -11,12 +11,12 @@ const Test = ({onSelectPokemon}) => {
                     <div className='flex flex-row'>
                         {data.pokemons.map((poke, idx) => (
                         <div onClick={() => onSelectPokemon(poke)} key={idx} className="flex items-center cursor-pointer mx-[-30px] w-53" onMouseEnter={() => setSelected(poke)} onMouseLeave={() => setSelected(null)}>
-                            <Link to='/pixel-art' state={{ pokemonId: idx,color: poke.colour, pokemon: poke["Pokemon-colour"]}} ><img src={poke.image} className="w-28 h-auto  opacity-50 hover:opacity-100"/></Link>
+                            <Link to='/pixel-art' state={{ pokemonId: idx,color: poke.colour, pokemon: poke["Pokemon-colour"]}} ><img src={import.meta.env.BASE_URL + poke.image.slice(1)} className="w-28 h-auto  opacity-50 hover:opacity-100"/></Link>
                         </div>
                         ))}
                     </div>
                     <div className="text-outline pixel1 text-[22px] text-white mt-6 bg-[#333333] rounded-l-lg p-4 w-[412px] min-h-[189px] mr-65">
-                        <img src="/public/Yapping.png" alt="Pfor tolkien" className="absolute ml-99 mt-[-16px]" />
+                        <img src={`${import.meta.env.BASE_URL}Yapping.png`} alt="Pfor tolkien" className="absolute ml-99 mt-[-16px]" />
                         {selected ? <p className="w-90 text-justify">{selected.des}</p> : <p className="w-90 text-justify">Venture above to shift thy creature, take heart! Thine deeds shall be preserved upon thy return.</p>}
                     </div>
                 </div>
